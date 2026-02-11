031287a021f35a3ba96f0a34865be1bbc460243a
Ghosuto <clash.raja10@gmail.com>
2026-02-11 08:11:50 +0000

    adapt 5 wifi icons for now
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- config.mk
+++ config.mk
@@ -232,6 +232,14 @@ endif
 PRODUCT_PACKAGES += \
     QSMedia
 
+# WiFi Icons
+PRODUCT_PACKAGES += \
+    AuroraWiFiOverlay \
+    XperiaWiFiOverlay \
+    LinearWiFiOverlay \
+    NothingDotWiFiOverlay \
+    ZigZagWiFiOverlay
+
 $(call inherit-product, vendor/extras/themes/bootanimation/bootanimation.mk)
 
 TARGET_PRODUCT_PROP += vendor/extras/gameprops/gameprops.prop
